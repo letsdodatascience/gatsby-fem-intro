2a15aa385d22aea87c8c74b2dfc540b6c7e00327
vickyID <vineet1.singh@citi.com>
2020-04-23 19:22:04 +0100

    PostPreview component for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from './../components/layout';
+import usePosts from './../hooks/use-posts';
 
-export default () => (
-  <Layout>
-    <h1>Hello Vineet Singh!</h1>
-    <p>Greetings from Dublin, Ireland</p>
-    <Link to="/about/">Learn about Me &rarr;</Link>
-  </Layout>
-);
+import PostPreview from '../components/post-preview';
+
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Hello Vineet Singh!</h1>
+      <p>Greetings from Dublin, Ireland</p>
+      <Link to="/about/">Learn about Me &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
