feb0c670b8247b1d50f26e48141f66c5fdcae7af
vickyID <vineet1.singh@citi.com>
2020-04-23 22:57:42 +0100

    Adding optimized images in the Hero section using Sharp with Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,25 @@ import { Link } from 'gatsby';
 import Layout from './../components/layout';
 import usePosts from './../hooks/use-posts';
 
+import Hero from './../components/hero';
 import PostPreview from '../components/post-preview';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Hello Vineet Singh!</h1>
-      <p>Greetings from Dublin, Ireland</p>
-      <Link to="/about/">Learn about Me &rarr;</Link>
+    <>
+      <Hero />
+      <Layout>
+        {/* <h1>Hello Vineet Singh!</h1>
+        <p>Greetings from Dublin, Ireland</p>
+        <Link to="/about/">Learn about Me &rarr;</Link> */}
 
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
