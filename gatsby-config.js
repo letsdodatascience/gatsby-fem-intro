69bb4d65392e6594d8aa6a4adfa7dd8cbb9eb1b8
vickyID <vineet1.singh@citi.com>
2020-04-23 17:34:39 +0100

    added site meta data using a graphql hook

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'My Blog',
+    description: 'Built during FEM workshop !!',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,70 +1,82 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import { Helmet } from 'react-helmet'; // for SEO
+
 import Header from './header';
+import useSiteMetadata from '../hooks/site-metadata';
 
-const Layout = ({ children }) => (
-  <React.Fragment>
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-        }
-
-        * + * {
-          margin-top: 1rem;
-        }
-
-        html,
-        body {
-          margin: 0;
-          color: #555;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-            'Segoe UI Symbol';
-          font-size: 18px;
-          line-height: 1.4;
-        }
-        /* remove the margin that gatsby mounts into*/
-        > div {
-          margin-top: 0;
-        }
-
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          color: #222;
-          line-height: 1.1;
-
-          + * {
-            margin-top: 0.5rem;
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <React.Fragment>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
           }
 
-          strong {
+          * + * {
+            margin-top: 1rem;
+          }
+
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+              'Segoe UI Emoji', 'Segoe UI Symbol';
+            font-size: 18px;
+            line-height: 1.4;
+          }
+          /* remove the margin that gatsby mounts into*/
+          > div {
+            margin-top: 0;
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
             color: #222;
-          }
+            line-height: 1.1;
 
-          li {
-            margin-top: 0.25rem;
-          }
-        }
-      `}
-    />
+            + * {
+              margin-top: 0.5rem;
+            }
 
-    <Header />
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 90vw;
-        width: 550px;
-      `}
-    >
-      {children}
-    </main>
-  </React.Fragment>
-);
+            strong {
+              color: #222;
+            }
+
+            li {
+              margin-top: 0.25rem;
+            }
+          }
+        `}
+      />
+
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </React.Fragment>
+  );
+};
 
 export default Layout;
