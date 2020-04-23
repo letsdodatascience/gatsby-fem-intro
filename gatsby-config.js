d791e97d97268d85e69828a341a494369b10dbca
vickyID <vineet1.singh@citi.com>
2020-04-23 18:20:21 +0100

    add contact mdx page with a simple react component

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'My Blog',
     description: 'Built during FEM workshop !!',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
